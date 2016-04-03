a5759f44aed87b49fdd54bf4887782f0ccb26f6a
AntonioMateus <antoniocms1995@hotmail.com>
2016-04-02 22:33:51 -0300

    codigos comentados

--- Part.java
+++ Part.java
@@ -2,13 +2,23 @@ import java.util.Map;
 import java.rmi.Remote; 
 import java.rmi.RemoteException; 
 
+/*
+* Interface Part
+* Comentario: representa cada uma das pecas. 
+*/
+
 public interface Part extends Remote {
-	public int getCodigo() throws RemoteException;
-	public String getNome() throws RemoteException; 
-	public String getDescricao() throws RemoteException;
-	public Map<Part,Integer> getComponentes() throws RemoteException;
-	public void setNome(String nome) throws RemoteException;
-	public void setDescricao(String descricao) throws RemoteException; 
-	public void adicionaSubComponente(Part subpeca, int quantidade) throws RemoteException;
-	public boolean ehPrimitiva() throws RemoteException;
+	public int getCodigo() throws RemoteException; //retorna o codigo da peca que esta sendo referenciada
+	public String getNome() throws RemoteException; //retorna o nome da peca que esta sendo referenciada 
+	public String getDescricao() throws RemoteException; //retorna a descricao da peca que esta sendo referenciada
+	public Map<Part,Integer> getComponentes() throws RemoteException; /*retorna os subcomponentes da peca que esta 
+	sendo referenciada. Tais subcomponentes sao representados por um mapa, em que cada par contem uma peca e a
+	correspondente quantidade. */
+	public void setNome(String nome) throws RemoteException; //modifica o nome da peca que esta sendo referenciada
+	public void setDescricao(String descricao) throws RemoteException; /*modifica a descricao da peca que esta
+	sendo referenciada.*/ 
+	public void adicionaSubComponente(Part subpeca, int quantidade) throws RemoteException; /*adiciona um
+	subcomponente a peca que esta sendo referenciada*/
+	public boolean ehPrimitiva() throws RemoteException; /*retorna se a peca eh primitiva (se nao possui
+	subcomponentes) ou nao */
 }
