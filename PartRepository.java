390c13fd1641e84c5f9abb70609dff94c6661d9c
AntonioMateus <antoniocms1995@hotmail.com>
2016-04-02 22:43:57 -0300

    codigos comentados

--- PartRepository.java
+++ PartRepository.java
@@ -3,11 +3,20 @@ import java.rmi.Remote;
 import java.rmi.RemoteException; 
 import java.util.Map; 
 
+/*
+* Interface PartRepository
+* Comentario: representa uma colecao de pecas
+*/
+
 public interface PartRepository extends Remote{
-	public Part inserePeca(String nome, String descricao) throws RemoteException;
-	public Part inserePeca(String nome, String descricao, Map<Part,Integer> subcomponentes) throws RemoteException;
-	public Part recuperaPeca(int id) throws RemoteException; 
-	public List<Part> recuperaTodasPecas() throws RemoteException; 
-	public String getNomeServidor() throws RemoteException; 
-	public int getNumeroPecas() throws RemoteException; 
+	//metodo que consiste em inserir uma peca na colecao
+	public Part inserePeca(String nome, String descricao) throws RemoteException; /*se a peca a ser inserida for primitiva, nao ha
+	necessidade de informar quais sao seus subcomponentes */
+	public Part inserePeca(String nome, String descricao, Map<Part,Integer> subcomponentes) throws RemoteException; /*se a peca a 
+	ser inserida for agregada, seus componentes serao passados como parametro */
+	public Part recuperaPeca(int id) throws RemoteException; //busca a peca cujo id foi informado como parametro
+	public List<Part> recuperaTodasPecas() throws RemoteException; /*retorna uma lista que contem todas as pecas armazenadas no
+	repositorio. */
+	public String getNomeServidor() throws RemoteException; //retorna o nome do servidor responsavel pelo repositorio 
+	public int getNumeroPecas() throws RemoteException; //retorna o numero de pecas armazenadas no repositorio
 }
